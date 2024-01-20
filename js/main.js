273d62290f81b309193f153132f250c21f435bcd
Anngel-0 <anngelo.omz@gmail.com>
2024-01-19 20:30:46 -0600

    mejorando estetica

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  function mostrar() {
-    Swal.fire("SweetAlert2 is working!");
-    console.log("picado");
-  }
+  const logo = document.querySelector(".logo");
+  logo.addEventListener("click", () => {
+    Swal.fire({
+      title: "¡Bienvenido a Anngelo Systems!",
+      html: "<p style='font-size: 18px;'>Siéntete libre de explorar mis proyectos y ponerte en contacto conmigo para colaboraciones o consultas.</p><p style='font-size: 18.5px;'>¡Gracias por visitar Anngelo Systems!</p>",
+      confirmButtonColor: "#327cae",
+      confirmButtonText: "Aceptar",
+    });
+        
+  });
 
   const certificados = document.querySelectorAll(".certificado-img");
   certificados.forEach((certificado) => {
@@ -32,16 +38,17 @@ document.addEventListener("DOMContentLoaded", function () {
     sessionStorage.removeItem("formularioEnviado");
   });
 
-  // Cuando la página se descarga o recarga
+  // Cuando la página se descarga (cerrar la pestaña, navegar a otra página, etc.). o recarga
   window.addEventListener("unload", function (event) {
     // Restablece el formulario al descargar o recargar la página
-    formulario.reset();
+    if (sessionStorage.getItem("formularioEnviado")) {
+      formulario.reset();
+    }
   });
-  
-  // Se activa antes de descargar la página
+
+  // Se activa antes de descargar o cerrar la página
   window.addEventListener("beforeunload", function (event) {
     // Establecer la señal "formularioEnviado" en sessionStorage al enviar el formulario
     sessionStorage.setItem("formularioEnviado", "true");
   });
-  
 });
--- js/removeStyle.js
+++ js/removeStyle.js
@@ -1,3 +1,4 @@
+//jQuery para ejecutar una función cuando el documento HTML ha sido completamente cargado
 $(document).ready(function() {
     var divStyle = $(".bi").prop("style");
         divStyle.removeProperty("background-color");
